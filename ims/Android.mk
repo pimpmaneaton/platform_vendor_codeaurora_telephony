LineageOS/android_vendor_codeaurora_telephony

telephony: Allow devices to provide their own telephony-ext
* Pixel devices specifically ship their own variants of telephony-ext
  and ims-common-ext.

Change-Id: I610d28aa8baaaef4ae4eeb2dcdc115335e3d9b73

ims/Android.mk
@@ -1,3 +1,5 @@
+ifneq ($(TARGET_PROVIDES_TELEPHONY_EXT),)
+
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -16,3 +18,5 @@ include $(BUILD_STATIC_JAVA_LIBRARY)
 # Include subdirectory makefiles
 # ============================================================
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+endif

pimpmaneaton/platform_vendor_codeaurora_telephony

ims/Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(TARGET_PROVIDES_TELEPHONY_EXT),)
+
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -16,3 +18,5 @@ include $(BUILD_STATIC_JAVA_LIBRARY)
 # Include subdirectory makefiles
 # ============================================================
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+endif